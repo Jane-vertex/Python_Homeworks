4e84c1915c0f95383fd73dcab997b85e9ad90778
Евгения <jennivertex@gmail.com>
2023-04-01 03:39:48 +0300

    change Task3

--- Tasks_HomeWork1/Task3-HW1/Task3.py
+++ Tasks_HomeWork1/Task3-HW1/Task3.py
@@ -19,3 +19,18 @@ if firstsSumNum == secondSumNum:
     print(f"сумма первых трех чисел числа {num} = сумме последних трех чисел и равна {secondSumNum}, так что yes")
 else:
     print("no")
+
+
+# решение преподавателя
+
+# n = int(input())
+# n1 = n // 100000
+# n2 = (n % 100000) // 10000
+# n3 = (n % 10000) // 1000
+# n4 = (n % 1000) // 100
+# n5 = (n % 100) // 10
+# n6 = n % 10
+# if n1 + n2 + n3 == n4 + n5 + n6:
+# print('yes')
+# else:
+# print('no')
